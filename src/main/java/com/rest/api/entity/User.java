6703307e16a0e60722e60cdbc2343be4f6a473f0
daniellee <khlee@thecheck.co.kr>
2021-02-07 21:25:16 +0900

    SpringBoot2로 Rest Api 만들기(5)

--- src/main/java/com/rest/api/entity/User.java
+++ src/main/java/com/rest/api/entity/User.java
@@ -17,10 +17,13 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private long msrl;
 
     @Column(nullable = false, unique = true, length = 30)
     private String uid;
+
+    @Column(nullable = false, length = 100)
+    private String name;
 
 
 
